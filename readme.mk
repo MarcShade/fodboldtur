1dd0634c7f64bdf6dfb7000c69b67c4bab2cc993
MarcShade <marcsorensenmail@gmail.com>
2025-10-26 22:00:46 +0100

    fixed readme url

--- readme.mk
+++ readme.mk
@@ -1,2 +1,2 @@
 Her ses flowchartet for den oprindelige udgave af koden som blev udleveret i opgavebeskrivelsen:
-![alt text](https://github.com/MarcShade/fodboldtur/blob/readme_images/oprindeligt_program.jpg "Snapshot of the game")
+![alt text](https://github.com/MarcShade/fodboldtur/blob/main/readme_images/oprindeligt_program.jpg "Snapshot of the game")
